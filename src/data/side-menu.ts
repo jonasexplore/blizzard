0d51a344e52155422ad9162f8e62aed6db338bb3
Jonas Brito <jonasexplore@gmail.com>
2022-12-03 07:57:14 -0300

    chore(data): added side menu items

---
+++ src/data/side-menu.ts
@@ -0,0 +1,7 @@
+export const sideMenuItems = [
+  "./assets/banner-hero/icons/game-1.png",
+  "./assets/banner-hero/icons/game-2.png",
+  "./assets/banner-hero/icons/game-3.png",
+  "./assets/banner-hero/icons/game-4.png",
+  "./assets/banner-hero/icons/game-5.png",
+];
