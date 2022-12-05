a5a3d59c0cf0e2c03835820c69d6bdc7486888fa
Jonas Brito <jonasexplore@gmail.com>
2022-12-05 19:05:04 -0300

    chore(data): added more static data

---
+++ src/data/submenu-games.ts
@@ -0,0 +1,22 @@
+export const SubmenuGamesItems = [
+  {
+    image: "./assets/icons/menu.svg",
+    width: 8,
+    label: "Ver todos jogos",
+  },
+  {
+    image: "./assets/icons/logo.svg",
+    width: 24,
+    label: "Aplicativo Battle.net",
+  },
+  {
+    image: "./assets/icons/downloads.svg",
+    width: 24,
+    label: "Downloads",
+  },
+  {
+    image: "./assets/icons/chat.svg",
+    width: 24,
+    label: "Fóruns dos jogos",
+  },
+];
---
+++ src/data/submenu-sports.ts
@@ -0,0 +1,22 @@
+export const SubmenuSports = [
+  {
+    image: "./assets/menu/icons/sports/hearthstone.png",
+    title: "Hearthstone® masters",
+  },
+  {
+    image: "./assets/menu/icons/sports/wow.png",
+    title: "Campeonato Mundial de Arena WoW®",
+  },
+  {
+    image: "./assets/menu/icons/sports/startcraft-ii.png",
+    title: "StarCraft® II WCS",
+  },
+  {
+    image: "./assets/menu/icons/sports/overwatch.png",
+    title: "Copa Mundial de Overwatch®",
+  },
+  {
+    image: "./assets/menu/icons/sports/overwatch-league.png",
+    title: "Liga de Overwatch®",
+  },
+];
